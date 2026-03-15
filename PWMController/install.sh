6d32031481ce9a83183eb55f7a7308cf3c6c33aa
BSP Porter <port@example.com>
2026-03-15 14:42:36 +0800

    Revert modified BSP files to upstream MangDang versions

--- PWMController/install.sh
+++ PWMController/install.sh
@@ -5,7 +5,5 @@
 set -x
 dtc i2c-pwm-pca9685a.dts > i2c-pwm-pca9685a.dtbo
 sudo cp i2c-pwm-pca9685a.dtbo /boot/firmware/overlays/
-# Note: Module compilation is handled by DKMS in prepare_dkms.sh
-# Skip local make commands to avoid conflicts with DKMS-installed modules
-echo "PWM controller device tree overlay installed. Driver compiled via DKMS."
-
+make
+make install
--- Python_Module/MangDang/mini_pupper/nvram.py
+++ Python_Module/MangDang/mini_pupper/nvram.py
@@ -1,18 +1,6 @@
-import os
 import pickle
 
-
-def _get_nvmem_path():
-    for path in [
-        '/sys/bus/nvmem/devices/3-00501/nvmem',  # Noble
-        '/sys/bus/nvmem/devices/3-00500/nvmem',  # Jammy
-    ]:
-        if os.path.exists(path):
-            return path
-    return '/sys/bus/nvmem/devices/3-00500/nvmem'
-
-
-ServoCalibrationFilePath = _get_nvmem_path()
+ServoCalibrationFilePath = '/sys/bus/nvmem/devices/3-00500/nvmem'
 
 
 def write(data):
